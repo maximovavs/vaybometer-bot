cbc543ed9410e7ef3ae543ed66bd74958ebf0c6d
maximovavs <maximovavs@gmail.com>
2026-07-31 08:59:25 -0700

    fix: activate Cyprus image content guard

--- world_en/__init__.py
+++ world_en/__init__.py
@@ -1,1 +1,9 @@
+"""World image-generation package hooks."""
 
+from . import imagegen as _imagegen
+from .image_content_guard import install_imagegen_guard as _install_imagegen_guard
+
+_install_imagegen_guard(_imagegen)
+
+del _imagegen
+del _install_imagegen_guard
